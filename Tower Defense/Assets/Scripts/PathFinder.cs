fa18358fd391781a2b3b5ab611cb0ae916ad7b65
Ivan Koniashov <87714602+Velsimir@users.noreply.github.com>
2022-07-29 00:14:06 +0300

    Realization Pathfinder
    Starting realization BFS

--- Tower Defense/Assets/Scripts/PathFinder.cs
+++ Tower Defense/Assets/Scripts/PathFinder.cs
@@ -3,13 +3,46 @@ using UnityEngine;
 
 public class PathFinder : MonoBehaviour
 {
+    [SerializeField] Waypoint startPoint, endPoint;
     Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+
+    Vector2Int[] directions =
+    {
+        Vector2Int.up,
+        Vector2Int.right,
+        Vector2Int.down,
+        Vector2Int.left
+    };
 
     void Start()
     {
         LoadBlocks();
+        SetColorEndStart();
+        ExploreNearPoints();
     }
 
+    void ExploreNearPoints()
+    {
+        foreach (Vector2Int direction in directions)
+        {
+            Vector2Int nearPointCoordinates = startPoint.GetGridPos() + direction;
+            try
+            {
+                grid[nearPointCoordinates].SetTopColor(Color.blue);
+            }
+            catch
+            {
+                Debug.LogWarning("Куб: " + nearPointCoordinates + "отсутствует");
+            }
+            print("проверено"+nearPointCoordinates);
+        }
+    }
+
+    void SetColorEndStart()
+    {
+        startPoint.SetTopColor(Color.green);
+        endPoint.SetTopColor(Color.black);
+    }
     private void LoadBlocks()
     {
         var waypoints = FindObjectsOfType<Waypoint>();
@@ -22,7 +55,6 @@ public class PathFinder : MonoBehaviour
             else
             { 
                 grid.Add(waypoint.GetGridPos(), waypoint);
-                waypoint.SetTopColor(Color.black);
             }
         }
     }
