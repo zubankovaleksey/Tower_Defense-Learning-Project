babd54811186cfe1863e48a05bdb8a850edc68a4
Ivan Koniashov <87714602+Velsimir@users.noreply.github.com>
2022-07-29 23:24:22 +0300

    PathFinder
    Alpha release of BFS Logic

--- Tower Defense/Assets/Scripts/PathFinder.cs
+++ Tower Defense/Assets/Scripts/PathFinder.cs
@@ -5,6 +5,8 @@ public class PathFinder : MonoBehaviour
 {
     [SerializeField] Waypoint startPoint, endPoint;
     Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+    Queue<Waypoint> queue = new Queue<Waypoint>();
+    bool isRuning = true;
 
     Vector2Int[] directions =
     {
@@ -17,35 +19,90 @@ public class PathFinder : MonoBehaviour
     void Start()
     {
         LoadBlocks();
-        SetColorEndStart();
-        ExploreNearPoints();
+        PathFind();
+        //ExploreNearPoints();
     }
 
-    void ExploreNearPoints()
+    //Метод поиска пути, от начальной точки
+    void PathFind()
     {
-        foreach (Vector2Int direction in directions)
+        queue.Enqueue(startPoint);
+        while (queue.Count > 0 && isRuning == true)
         {
-            Vector2Int nearPointCoordinates = startPoint.GetGridPos() + direction;
-            try
-            {
-                grid[nearPointCoordinates].SetTopColor(Color.blue);
-            }
-            catch
-            {
-                Debug.LogWarning("Куб: " + nearPointCoordinates + "отсутствует");
-            }
-            print("проверено"+nearPointCoordinates);
+            Waypoint searchPoint = queue.Dequeue();
+            searchPoint.isExplored = true;
+            print("Исследовать соседние клетки из: " + searchPoint);
+            CheckForEndpoint(searchPoint);
+            ExploreNearPoints(searchPoint);
+        }
+        SetColorEndStart();
+        print("Можем сгенерировать путь?");
+    }
+
+    //Метод объявляющий о нахождении финальной точки
+    void CheckForEndpoint(Waypoint searchPoint)
+    {
+        if (searchPoint == endPoint)
+        {
+            print("Алгоритм нашел финальную точку");
+            isRuning = false;
         }
     }
 
+    //Метод исследующий ближайшую точку, до нахождения финальной точки
+    void ExploreNearPoints(Waypoint from)
+    {
+        if (!isRuning)
+        {
+            return;
+        }
+        
+        foreach (Vector2Int direction in directions)
+        {
+            Vector2Int nearPointCoordinates = from.GetGridPos() + direction;
+
+            try
+            {
+                Waypoint nearPoint = grid[nearPointCoordinates];
+                AddPointQuee(nearPoint);
+            }
+            catch
+            {
+
+            }
+        }
+    }
+
+    //Метод добавляющий точку в очередь
+    void AddPointQuee(Waypoint nearPoint)
+    {
+        //Если ближайшая не точка исследована, добавляем ее в очередь
+        if (nearPoint.isExplored)
+        {
+            return;
+        }
+        else
+        {
+            print("Добавить в очередь: " + nearPoint);
+            nearPoint.SetTopColor(Color.blue);
+            queue.Enqueue(nearPoint);
+        }
+    }
+
+    //Метод устанавливающий цвета для начальной и конечной точек
     void SetColorEndStart()
     {
         startPoint.SetTopColor(Color.green);
         endPoint.SetTopColor(Color.black);
     }
-    private void LoadBlocks()
+
+    //Метод загрузки блоков в словарь
+    void LoadBlocks()
     {
+        //Поиск объектов с типом Waypoint
         var waypoints = FindObjectsOfType<Waypoint>();
+
+        //Цикл позволяющий помещающий найденные объекты в словарь
         foreach (Waypoint waypoint in waypoints)
         {
             Vector2Int gridPos = waypoint.GetGridPos();
--- Tower Defense/Assets/Scripts/WayPoint.cs
+++ Tower Defense/Assets/Scripts/WayPoint.cs
@@ -1,11 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
     Vector2Int gridPos;
     const int gridSize = 10;
+    public bool isExplored = false;
 
     public int GetGridSize()
     {
@@ -22,6 +21,7 @@ public class Waypoint : MonoBehaviour
             );
     }
 
+    //Метод перекрашивающий верхушку куба
     public void SetTopColor(Color color)
     {
         MeshRenderer topMeshRenderer = transform.Find("Quad Top").GetComponent<MeshRenderer>();
